e1c05c74bdfa582f222aad31d25da1738f23c60f
Arulselvam5006 <52314922+Arulselvam5006@users.noreply.github.com>
2019-07-01 11:44:43 +0530

    Update vowvels.py

--- vowvels.py
+++ vowvels.py
@@ -1,9 +1,8 @@
 str = input("")
-if str in ('A', 'E', 'I', 'O', 'U','a','e','i','o')
+if str in ('A', 'E', 'I', 'O', 'U','a','e','i','o'):
 	print("%s is a vowel." % str)
+
 	print("vowel")
-else
+else:
 	print("%s is a consonant." % str) 
- 
- 
  
